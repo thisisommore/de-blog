2203ec651f1b86a6a930522d4850c2438802882d
thisisommore <ommore501@gmail.com>
2023-11-28 16:08:36 +0000

    Header: add DeBlog as title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 const Header = () => {
   return (
     <header className="flex w-100 p-2">
+      <h2 className="text-2xl">DeBlog</h2>
       <div className="ml-auto">
         <ConnectButton />
       </div>
