06666183d2f42cf54f1b50dc0bb54bdf4967209c
thisisommore <ommore501@gmail.com>
2023-11-28 18:44:49 +0000

    header: link title to root(`/`) page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Link from "next/link";
 const Header = () => {
   return (
-    <header className="flex w-100 p-2">
-      <h2 className="text-2xl">DeBlog</h2>
+    <header className="flex w-100 p-2 border-b rounder-sm items-center">
+      <Link href="/">
+        <h2 className="text-2xl font-semibold">DeBlog</h2>
+      </Link>
       <div className="ml-auto">
         <ConnectButton />
       </div>
